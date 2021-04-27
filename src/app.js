4492427a0b9f1b8cfe0d4c0f6cabd4105f2a3f76
LaetitiaGullo <lg.laetitiagullo@gmail.com>
2021-04-27 15:23:52 +0200

    tooling

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-import HelloWorld from "./components/HelloWorld"
+//ReactDOM.render(<HelloWorld />, document.getElementById("root"));
 
-ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+console.log("Hello, World !")
